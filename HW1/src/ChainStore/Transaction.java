ed9f2cc2a1be5eefd4934d9951b8356fc0a17ec8
vonGradnell <47977176+vonGradnell@users.noreply.github.com>
2020-11-16 17:58:46 +0300

    Update Transaction.java

--- HW1/src/ChainStore/Transaction.java
+++ HW1/src/ChainStore/Transaction.java
@@ -2,8 +2,6 @@ package ChainStore;
 
 /**
  * Transaction object class
- * @author Onur Cihangir 250201049
- * @author Uğur Baran Özer 250201034
  */
 public class Transaction {
 	private double purchasePrice;
